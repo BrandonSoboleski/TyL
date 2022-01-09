c6fd3fac283f3e19689a7613b028db4b36d1de88
Tyler Kurzawa <tykur14@gmail.com>
2022-01-09 14:08:57 -0500

    Please fix the merge issue

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 #include <string>
-#include <iostream>
 
 int main(){ 
     std :: cout << "Hello World" << std :: endl;
-    return 0;
-}
-
-int main()
-{
-    std::cout << "Hello world" << std::endl;
     return 0;
 }
